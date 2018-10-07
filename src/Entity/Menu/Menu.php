378d5ccb5224c5226f53315fdbb7a8dcd90df82e
moulaye <moulaye.c@gmail.com>
2018-10-07 19:46:13 +0200

    Refactor Classes

--- src/Entity/Menu/Menu.php
+++ src/Entity/Menu/Menu.php
@@ -11,6 +11,12 @@ namespace App\Entity\Menu;
 
 use Cisse\Traits\Entity\IdTrait;
 
+/**
+ * Class Menu
+ * @package App\Entity\Menu
+ *
+ * @ORM\Entity(repositoryClass="App\Repository\Menu")
+ */
 class Menu
 {
     use IdTrait;
